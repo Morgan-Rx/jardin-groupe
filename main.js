e3f450a3ff61828ef73c9466bd6c5fcdfe6f9573
xtcp <xtcp.mail@gmail.com>
2026-04-10 16:27:25 +0200

    Plant function correction

--- main.js
+++ main.js
@@ -14,15 +14,29 @@ import * as Globals from './js/variables.js'
 
         const gridDiv = document.getElementById("grid");
 
+        let watering = false;
+
+        const burronArroser = document.getElementById("arroser");
+        burronArroser.addEventListener("click", function() {
+            watering = true;
+            const tSquares = document.querySelectorAll(".square");
+            tSquares.forEach(tSquare => {
+                
+                tSquare.style.cursor = "url('public/assets/images/watering.webp'), auto";
+                console.log(tSquare.style.cursor);
+            });
+        })
         function openPopup(id, y, x) {
 
         }
         function plantFlower(id, flower) {
             const tSquare = document.getElementById(id);
             console.log(tSquare);
-            tSquare.style.backgroundImage = `url("public/assets/images/fleur${flower}.webp")`;
-            tSquare.style.backgroundPosition = "0px 80px";
-            tSquare.style.backgroundSize = "300px";
+            tSquare.style.backgroundImage = `url("public/assets/images/flower-${flower}-1.webp")`;
+            tSquare.style.backgroundPosition = "left center";
+            tSquare.style.backgroundSize = "34px";
+            tSquare.style.backgroundRepeat = "no-repeat";
+
         }
 
         for (let i = 1; i <= gridSize[1]; i++) {
@@ -36,7 +50,18 @@ import * as Globals from './js/variables.js'
                 fragment.addEventListener("click",  function() {
                     openPopup(((i-1)*gridSize[0])+h, i, h);
                     console.log(((i-1)*gridSize[0])+h);
-                    plantFlower(((i-1)*gridSize[0])+h, Math.floor(Math.random()*5)+1); 
+                    if (watering) {
+                        watering = false;
+                        const tSquares = document.querySelectorAll(".square");
+                        tSquares.forEach(tSquare => {
+                            tSquare.style.cursor =  "pointer";
+                        });
+                        fragment.style.backgroundImage = `url("public/assets/images/flower-1-3.webp")`;
+                        console.log(url("public/assets/images/flower-${flower}-3.webp"));
+                    } else {
+                        plantFlower(((i-1)*gridSize[0])+h, Math.floor(Math.random()*3)+1); 
+                    }
+                    
                 })
                 console.log(((i-1)*gridSize[0]));
                 fragment.innerHTML = "";
