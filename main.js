716e2ff95ea732c01bb1f44ff9c1ed6eed699f44
xtcp <xtcp.mail@gmail.com>
2026-04-10 15:38:09 +0200

    Plant function

--- main.js
+++ main.js
@@ -10,32 +10,43 @@ import * as Globals from './js/variables.js'
     if(state === 'interactive' || state === 'complete') {
 
         
-        const gridSize = [8, 3];
+        const gridSize = [6, 3];
 
         const gridDiv = document.getElementById("grid");
 
         function openPopup(id, y, x) {
-            
+
+        }
+        function plantFlower(id, flower) {
+            const tSquare = document.getElementById(id);
+            console.log(tSquare);
+            tSquare.style.backgroundImage = `url("public/assets/images/fleur${flower}.webp")`;
+            tSquare.style.backgroundPosition = "0px 80px";
+            tSquare.style.backgroundSize = "300px";
         }
 
-        for (let i = 0; i <= gridSize[1]; i++) {
+        for (let i = 1; i <= gridSize[1]; i++) {
 
-            for (let h = 0; h <= gridSize[0]; h++) {
+            for (let h = 1; h <= gridSize[0]; h++) {
 
                 const fragment = document.createElement("div");
-                fragment.setAttribute("data-id", i*h);
+                fragment.id = ((i-1)*gridSize[0])+h;
                 fragment.setAttribute("data-row", i);
                 fragment.setAttribute("data-pos", h);
                 fragment.addEventListener("click",  function() {
-                    openPopup(i*h, i, h);    
+                    openPopup(((i-1)*gridSize[0])+h, i, h);
+                    console.log(((i-1)*gridSize[0])+h);
+                    plantFlower(((i-1)*gridSize[0])+h, Math.floor(Math.random()*5)+1); 
                 })
-                console.log(i);
+                console.log(((i-1)*gridSize[0]));
                 fragment.innerHTML = "";
                 fragment.className = "square";
                 gridDiv.appendChild(fragment);
             }
-            
         }
+
+
+        plantFlower(1, "1");
     }
     else setTimeout(arguments.callee, 100);
 })();
